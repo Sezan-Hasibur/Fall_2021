c980a10c97efafc30daa8f2b0dead9eda77cba10
Hasibur Rahman Sazen <60995411+Sezan-Hasibur@users.noreply.github.com>
2021-11-09 14:40:28 +0600

    Create page1.php

---
+++ LabExam/page1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+<fieldset>
+<h1><b>Personal Details</b></h1>
+<hr>
+<form action=" " method="POST">
+<table>
+    
+<tr><td>User name: <input type="text" name="username"> 
+</td></tr>
+<tr><td>Password:   <input type="password" name="password"> 
+</td></tr>
+<tr><td>Full Name: <input type="text" name="fullname">
+</td></tr>
+<tr><td>Mobile No: <input type="text" name="mobno"> 
+</td></tr>
+<tr><td>Date of Birth: <input type="date" name="bdy"> 
+</td></tr>
+<td><a href ="page2.php"><input type="button" value="Next"> 
+</a></td>
+
+</tabel>
+</form>
+</fieldset>
+</body>
+</html>
