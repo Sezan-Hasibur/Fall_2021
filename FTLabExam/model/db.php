8732bf5a4fa853683aef374ad27820e99b72e67b
Hasibur Rahman Sazen <60995411+Sezan-Hasibur@users.noreply.github.com>
2021-12-06 12:43:17 +0600

    Update db.php

--- FTLabExam/model/db.php
+++ FTLabExam/model/db.php
@@ -1,1 +1,44 @@
+<?php
+class db{
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "mydb";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
+ 
+ return $conn;
+ }
 
+function SearchName($conn,$table,$name) // Search using table name
+ {
+$result = $conn->query("SELECT * FROM $table WHERE Name='$name'");
+ return $result;
+ }
+
+function ShowAll($conn,$table) // Show all data from table
+ {
+$result = $conn->query("SELECT * FROM  $table");
+ return $result;
+ }
+
+ function SearchInterest($conn,$table,$interest) // Search using table interset
+ {
+$result = $conn->query("SELECT * FROM $table WHERE Interest='$interest'");
+ return $result;
+}
+
+ function SearchDesignation($conn,$table,$udesignation) // Search using table designation
+ {
+$result = $conn->query("SELECT * FROM $table WHERE Designation='$designation'");
+ return $result;
+}
+
+
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+}
+?>
